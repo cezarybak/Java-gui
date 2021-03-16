aedababdd763fe76cc518564e934cf19803ba53d
Cezary Bąk <cezarybak@MBP-Cezary.home>
2021-03-16 01:14:30 +0100

    14-15.03

--- src/Day2Zad4/MainDay2Zad4.java
+++ src/Day2Zad4/MainDay2Zad4.java
@@ -17,8 +17,21 @@ public class MainDay2Zad4 {
 
 
         System.out.println(fig[1].pole());
+        System.out.println(fig[1].obwód());
+
+        System.out.println(fig[0].pole());
         System.out.println(fig[0].obwód());
 
+        Figura p2 = new Prostokat2(20, 20, 10, 5, '*');
+        ((Prostokat2)p2).rysuj();
+
+        Kolo2 k2 = new Kolo2(15, 20, 5);
+
+        k2.przesunDo(50, 40);    // przesunięcie środka koła do punktu (50, 40)
+        System.out.println(k2);
+
+        k2.powrot();                    // powrót do poprzedniej pozycji (bezpośrednio przed przesunięciem) środka koła
+        System.out.println(k2);
 
     }
 }
@@ -46,14 +59,10 @@ abstract class Figura implements Obliczenie {
     }
 
     @Override
-    public int pole() {
-        return 0;
-    }
+    public abstract double pole();
 
     @Override
-    public int obwód() {
-        return 0;
-    }
+    public abstract double obwód();
 }
 
 class Kolo extends Figura implements Obliczenie {
@@ -96,6 +105,41 @@ class Kolo extends Figura implements Obliczenie {
     public String toString()
     {
         return super.toString() + "\nŚrodek - (" + x + ',' + y + ")" + "\nPromień: " + promien + "\n";
+    }
+
+    @Override
+    public double obwód() {
+        return (2 * Math.PI * promien);
+    }
+
+    @Override
+    public double pole() {
+        return (Math.pow(promien, 2) * Math.PI);
+    }
+}
+
+class Kolo2 extends Kolo implements Transformacja{
+
+    private int helperx, helpery;
+
+    public Kolo2(int x, int y, int r) {
+        super(x, y, r);
+    }
+
+    @Override
+    public void przesunDo(int x, int y) {
+        helperx = this.x;
+        helpery = this.y;
+
+        this.x = x;
+        this.y = y;
+
+    }
+
+    @Override
+    public void powrot() {
+        x = helperx;
+        y = helpery;
     }
 
 
@@ -144,9 +188,49 @@ class Prostokat extends Figura implements Obliczenie {
         return super.toString() + "\nLewy górny - (" + x + ',' + y + ")" + "\nSzerokość: " + szer + ", " + "Wysokość: " + wys + "\n";
     }
 
+    @Override
+    public double pole() {
+        return (szer*wys);
+    }
+
+    @Override
+    public double obwód() {
+        return (2*szer + 2*wys);
+    }
+}
+
+class Prostokat2 extends Prostokat implements  Rysowanie{
+
+    public char c;
+
+
+    public Prostokat2(int x, int y, int s, int w, char c) {
+        super(x, y, s, w);
+        this.c = c;
+    }
+
+    @Override
+    public void rysuj() {
+        for(int x = 0; x < wys; x++){
+            for(int y = 0; y <  szer; y++){
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
 }
 
 interface Obliczenie{
-    public  int obwód();
-    public  int pole();
+    public double obwód();
+    public double pole();
+}
+
+interface Rysowanie{
+    public void rysuj();
+}
+
+interface Transformacja{
+
+    public void przesunDo(int x, int y);
+    public void powrot();
 }
