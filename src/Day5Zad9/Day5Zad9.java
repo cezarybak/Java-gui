8e18d8de9a13d456245eda67bcaed77efeb0a735
Cezary Bąk <cezarybak@MBP-Cezary.home>
2021-04-17 22:29:01 +0200

    Day 5 1/2

--- src/Day5Zad9/Day5Zad9.java
+++ src/Day5Zad9/Day5Zad9.java
@@ -1,29 +1,25 @@
 package Day5Zad9;
 
+import java.util.Random;
+
 public class Day5Zad9 {
 
     public static void main(String[] args)
     {
-        Player p1 = new Player("ppj");                                            // tworzenie gracza ze swoim identyfikatorem
+        Player p1 = new Player("ppj");
         Player p2 = new Player("gui");
 
-        Referee ref = new Referee(10, new Player[]{p1,p2});       //  arbiter ustala czas gry (w sekundach), "rejestruje" tablicę graczy
+        Referee ref = new Referee(10, new Player[]{p1,p2});
 
-        ref.startGame();                                                                   // arbiter startuje swój wątek: mierzy czas oraz daje sygnał startu graczom
+        ref.startGame();
 
         try {
-            ref.join();                                                                          // czekamy, aż wątek arbitra zakończy swoją pracę, tzn. po upływie określonego czasu przerywa pracę wątków wszystkich graczy
-
-            // join() jest metodą z klasy Thread
+            ref.join();
 
         } catch (InterruptedException exc){
-
             exc.printStackTrace();
-
         }
-
-        ref.result();                                                                            // arbiter ogłasza wynik gry
-
+        ref.result();
     }
 }
 
@@ -33,43 +29,82 @@ class Player extends Thread {
     public String name;
     public int score = 0;
 
-    public Player(String name)
-    {
+
+    public Player(String name) {
         this.name = name;
     }
 
-    public void run()
-    {
 
+    public void run() {
+        int howmanytime = 1;
+
+        while (true) {
+
+            int delay = new Random().nextInt(2000) + 1;
+            int score = new Random().nextInt(100) + 1;
+
+
+            System.out.println("Czas: " + howmanytime);
+            System.out.println(this.name + ": " + score);
+
+            try {
+                sleep(delay);
+            } catch (InterruptedException e) {
+                return;
+            }
+            howmanytime++;
+            this.score += score;
+        }
     }
-
 }
-
 
 class Referee extends Thread {
 
-    private int time;
-    private  Player[] players;
+    private long time;
+    private Player[] players;
 
-    public Referee(int time, Player[] players) {
+    public Referee(int time, Player[] players)
+    {
+        this.time = time;
         this.players = players;
-
     }
 
-    public void startGame() {
+    void startGame(){
 
-    }
 
-    public void result() {
 
-        System.out.println("wyniki " + players[0].name + ": " + players[0].score);
-        System.out.println("wyniki " + players[1].name + ": " + players[1].score);
-        System.out.println();
-        if(players[0].score > players[1].score){
-            System.out.println(players[0].score);
-        }else{
-            System.out.println(players[1].score);
+        players[0].start();
+        players[1].start();
+
+        try {
+            sleep(time * 1000);
+        } catch (InterruptedException e) {
+            return;
         }
+        players[0].interrupt();
+        players[1].interrupt();
 
+
+    }
+
+
+
+
+    void result() {
+        System.out.println();
+
+        for(int x = 0; x < players.length; x++){
+            System.out.println("wyniki: " + players[x].name + ": " + players[x].score);
+
+        }
+        System.out.println();
+
+        if(players[0].score > players[1].score){
+            System.out.println(players[0].name + " wygrał!");
+        }else if(players[1].score > players[0].score){
+            System.out.println(players[1].name + " wygrał!");
+        }else{
+            System.out.println("Remis");
+        }
     }
 }
